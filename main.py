8e06037484fa4fc2e5bacbd1659013bf8bf2ad7f
Lukaribou <lucasperroud17@gmail.com>
2020-04-20 17:32:45 +0200

    Tentative boucles imbriquées

--- main.py
+++ main.py
@@ -1,61 +1,61 @@
 tableau_val = [0]
-parcours, pointeur = 0, 0
-code = ""
+code = ''
 
 
 def lire():
     with open('./bf.txt', 'r') as fichier:
         global code
-        code = "".join(fichier.readlines())
-        nettoyer()
-
-
-def nettoyer():
-    global code
-    code = ''.join(filter(lambda x: x in ['.', ',', '[', ']', '<', '>', '+', '-'], code))
+        code = ''.join(fichier.readlines())
+        code = ''.join(filter(lambda x: x in ['.', ',', '[', ']', '<', '>', '+', '-'], code))
 
 
 def executer():
-    global code, pointeur, parcours
-    boucle = trouver_boucle()
+    pointeur, parcours, boucle_cptr = 0, 0, 0
+    boucles = trouver_boucle()
 
     while parcours < len(code):
         signe = code[parcours]
-        if signe == "+":
+        if signe == '+':
             tableau_val[pointeur] += 1 if tableau_val[pointeur] < 255 else 0
-        elif signe == "-":
+        elif signe == '-':
             if tableau_val[pointeur] > 0:
                 tableau_val[pointeur] -= 1
-        elif signe == ">":
+        elif signe == '>':
             if pointeur + 1 == len(tableau_val):
                 tableau_val.append(0)
             pointeur += 1
-        elif signe == "<":
+        elif signe == '<':
             if not pointeur - 1 == -1:
                 pointeur -= 1
-        elif signe == ".":
-            print(chr(tableau_val[pointeur]), end="")
-        elif signe == ",":
+        elif signe == '.':
+            print(chr(tableau_val[pointeur]), end='')
+        elif signe == ',':
             try:
-                tableau_val[pointeur] = int(input("Entrée byte: "))
+                tableau_val[pointeur] = int(input('Entree byte: '))
             except ValueError:
-                print("Une erreur est survenue. L'entrée attendue est un nombre entre 0 et 255 !")
-        elif signe == "[" and tableau_val[pointeur] == 0:
-            parcours = boucle["fin"]
-        elif signe == "]" and tableau_val[pointeur] != 0:
-            parcours = boucle["debut"]
+                print('Une erreur est survenue. L\'entree attendue est un nombre entre 0 et 255 !')
+        elif signe == '[':
+            boucle_cptr += 1
+        elif signe == ']':
+            if tableau_val[pointeur] != 0:
+                parcours = boucles[boucle_cptr - 1]['debut']
+            else:
+                boucle_cptr -= 1
 
         parcours += 1
 
 
 def trouver_boucle():
-    global code
-    boucle = {}
+    boucle, temp, cpt = [], [], 0
     for index, signe in enumerate(code):
-        if signe == "[":
-            boucle["debut"] = index
-        if signe == "]":
-            boucle["fin"] = index
+        print(cpt)
+        if signe == '[':
+            temp.append(index)
+            cpt += 1
+        elif signe == ']':
+            debut = temp.pop()
+            boucle[cpt - 1] = {'debut': debut, 'fin': index}
+    print(boucle)
     return boucle
 
 
