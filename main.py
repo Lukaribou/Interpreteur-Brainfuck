aa14d7c1b0b9627735a1fc071e775f201dd9398e
Lukaribou <lucasperroud17@gmail.com>
2020-05-25 13:12:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,55 +2,65 @@
 
 def lire(file_name: str) -> str:
     with open(file_name, 'r') as fichier:
-        print(f"Lecture de \"{file_name}\"...")
-        return "".join(fichier.readlines())
+        print(f'Lecture de \"{file_name}\"...')
+        return ''.join(fichier.readlines())
 
 
 def nettoyer(code: str) -> str:
-    print("Nettoyage du code...")
+    print('Nettoyage du code...')
     return ''.join(filter(lambda x: x in ['.', ',', '[', ']', '<', '>', '+', '-'], code))
 
 
-def executer(code) -> None:
-    print("Interpretation...\n")
-    parcours, pointeur, tableau_val = 0, 0, [0]
-    boucle = trouver_boucle(code)
+def executer(code: str) -> None:
+    print('Interpretation...\n')
+    parcours, pointeur, tableau_val, boucle_cptr = 0, 0, [0], 0
+    boucles = trouver_boucle(code)
 
     while parcours < len(code):
         signe = code[parcours]
-        if signe == "+":
+        if signe == '+':
             tableau_val[pointeur] += 1 if tableau_val[pointeur] < 255 else -255  # 255 += -255 = -255
         elif signe == '-':
             tableau_val[pointeur] -= 1 if tableau_val[pointeur] > 0 else -255  # 0 -= -255 = 255
-        elif signe == ">":
+        elif signe == '>':
             if pointeur + 1 == len(tableau_val):
                 tableau_val.append(0)
             pointeur += 1
-        elif signe == "<" and not pointeur == 0:
+        elif signe == '<' and not pointeur == 0:
             pointeur -= 1
-        elif signe == ".":
-            print(chr(tableau_val[pointeur]), end="")
-        elif signe == ",":
+        elif signe == '.':
+            print(chr(tableau_val[pointeur]), end='')
+        elif signe == ',':
             try:
-                tableau_val[pointeur] = int(input("Entrée byte: "))
+                tableau_val[pointeur] = int(input('Entrée byte: '))
             except ValueError:
-                print("Une erreur est survenue. L'entrée attendue est un nombre entre 0 et 255 !")
+                print('Une erreur est survenue. L\'entrée attendue est un nombre entre 0 et 255 !')
                 break
-        elif signe == "[" and tableau_val[pointeur] == 0:
-            parcours = boucle["fin"]
-        elif signe == "]" and tableau_val[pointeur] != 0:
-            parcours = boucle["debut"]
+        elif signe == '[':
+            if tableau_val[pointeur] == 0:
+                parcours = boucles[boucle_cptr]['fin']
+                boucle_cptr -= 1
+            else:
+                boucle_cptr += 1
+        elif signe == ']' and tableau_val[pointeur] != 0:
+            parcours = boucles[boucle_cptr - 1]['debut']
 
         parcours += 1
 
 
-def trouver_boucle(code):
-    boucles = {}
+def trouver_boucle(code: str):
+    boucles, cptr = [], 0
     for index, signe in enumerate(code):
-        if signe == "[":
-            boucles["debut"] = index
-        if signe == "]":
-            boucles["fin"] = index
+        if signe == '[':
+            boucles.append({
+                'debut': index,
+                'fin': None
+            })
+            cptr += 1
+        if signe == ']':
+            cptr -= 1
+            boucles[cptr]['fin'] = index
+    print(boucles)
     return boucles
 
 
