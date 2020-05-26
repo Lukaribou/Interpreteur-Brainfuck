f68ae9aa68d60515218dc45de1d925167e58e503
Lukaribou <lucasperroud17@gmail.com>
2020-05-26 14:03:29 +0200

    Création du menu et de la fonction demander

--- menu.py
+++ menu.py
@@ -1,6 +1,30 @@
+def afficher_menu() -> None:
+    """Affiche le menu et attend un choix"""
+
+    print('\n** Menu **\n')
+    menu = ['Interpreter', 'Optimiser']
+    for k, v in enumerate(menu):
+        print(f'{k}: {v}')
+    print('')  # afficher une ligne vide entre la liste et la question
+
+    choix = demander('Choisissez l\'action que vous souhaitez faire sur votre fichier: ',
+                     lambda rep: True if str.isnumeric(rep) and 0 <= int(rep) < len(menu) else False)
+    # str.isnumeric() -> https://www.geeksforgeeks.org/python-string-isnumeric-application/
+    print('')  # afficher une ligne vide en dessous de la question
+
+
+def demander(question, verif=(lambda rep: True if str.lower(rep) in ['n', 'y'] else False)) -> str:
+    """Pose la question jusqu'à ce que la verif renvoie True
+
+    La verif de base attendant un y ou un n"""
+    while True:
+        rep = input(question)
+        if verif(rep):  # si la verif est bonne on retourne la valeur
+            return rep
 
 
 def lire(file_name: str) -> str:
+    """Lis le fichier et renvoie son contenu sous la forme d'une seule string"""
     with open(file_name, 'r') as fichier:
         print(f'Lecture de \"{file_name}\"...')
         return ''.join(fichier.readlines())
@@ -8,6 +32,6 @@ def lire(file_name: str) -> str:
 
 if __name__ == '__main__':
     import sys
-    from interpreteur import *
+
     cible = sys.argv[1] if len(sys.argv) > 1 else 'main.bf'
-    executer(nettoyer(lire(cible)))
+    afficher_menu()
